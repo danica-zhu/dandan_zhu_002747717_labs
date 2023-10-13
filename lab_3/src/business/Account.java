534cc9bb085ed2e6c8c530e7127fa76c221b49fb
danica-zhu <145221361+danica-zhu@users.noreply.github.com>
2023-10-12 22:55:42 -0400

    Add files via upload

---
+++ lab_3/src/business/Account.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business;
+
+import java.util.Date;
+
+/**
+ *
+ * @author danica
+ */
+public class Account {
+    
+    private String routingNumber;
+    private String accountNumber;
+    private String bankNumber;
+    private int balance;
+    private Date createdOn;
+
+    public Account() {
+        this.createdOn = new Date();
+    }
+    
+
+    public String getRoutingNumber() {
+        return routingNumber;
+    }
+
+    public void setRoutingNumber(String routingNumber) {
+        this.routingNumber = routingNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNUmber) {
+        this.accountNumber = accountNUmber;
+    }
+
+    public String getBankNumber() {
+        return bankNumber;
+    }
+
+    public void setBankNumber(String bankNumber) {
+        this.bankNumber = bankNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+    
+    public String toString(){
+        return bankNumber;
+    }
+    
+    
+}
---
+++ lab_3/src/business/AccountDirectory.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author danica
+ */
+public class AccountDirectory {
+
+    private ArrayList<Account> accountList;
+
+//    public AccountDirectory(ArrayList<Account> accountList) {
+//    }
+
+    public AccountDirectory() {
+        this.accountList = new ArrayList<Account>();
+
+    }
+
+
+    public ArrayList<Account> getAccountList() {
+        return accountList;
+    }
+
+    public void setAccountList(ArrayList<Account> accountList) {
+        this.accountList = accountList;
+    }
+    
+    public Account addAccount(){
+        Account account = new Account();
+        accountList.add(account);
+        return account;
+    }
+    
+    public void deleteAccount(Account account) {
+        accountList.remove(account);
+    }
+    
+    public Account searchAccount(String accountNumber) {
+        for (Account user:accountList) {
+            if (user.getAccountNumber().equals(accountNumber)) {
+                return user;
+            }
+        }
+        
+        return null;
+    }
+    
+    
+    
+}
