fe1f6fa36104fa22737b1f070ac63216e9345dc1
danica-zhu <145221361+danica-zhu@users.noreply.github.com>
2023-11-05 23:35:20 -0500

    first submit for lab-4

---
+++ Lab_4/src/model/FeatureVolumn.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author danica
+ */
+
+public class FeatureVolumn extends Feature {
+
+    private String name;
+    public FeatureVolumn(Product owner) {
+        super(owner);
+        this.name = "volumn";
+        this.setName(name);
+    }
+
+}
+
---
+++ Lab_4/src/model/FeatureWeight.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author danica
+ */
+
+public class FeatureWeight extends Feature {
+
+    private String name;
+    public FeatureWeight(Product owner) {
+        super(owner);
+        this.name = "weight";
+        this.setName(name);
+    }
+
+}
+
